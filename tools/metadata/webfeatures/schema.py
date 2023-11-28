9cb57ece62e8c3df238d43a075f2cb61c58e583d
James Scott <jamescscott@google.com>
2023-11-28 22:00:47 +0000

    Add schema for WEB_FEATURES.yml

---
+++ tools/metadata/webfeatures/schema.py
@@ -0,0 +1,91 @@
+from enum import Enum
+from dataclasses import dataclass
+from fnmatch import fnmatchcase
+from typing import Any, Sequence, Union
+
+from ..schema import SchemaValue, validate_dict
+
+"""
+YAML filename for meta files
+"""
+WEB_FEATURES_YML_FILENAME = "WEB_FEATURES.yml"
+
+
+class SpecialFileEnum(Enum):
+    """All files recursively"""
+    RECURSIVE = "**"
+
+
+class FeatureFile(str):
+    def match_files(self, base_filenames: Sequence[str]) -> Sequence[str]:
+        """
+        Given the input base file names, returns the subset of base file names
+        that match the given FeatureFile.
+        If the FeatureFile contains any number of "*" characters, fnmatch is
+        used check each file name.
+        If the FeatureFile does not contain any "*" characters, the base file name
+        must match the FeatureFile exactly
+        :param base_filenames: The list of filenames to check against the FeatureFile
+        :return: List of matching file names that match FeatureFile
+        """
+        result = []
+        # If our file name contains a wildcard, use fnmatch
+        if "*" in self:
+            for base_filename in base_filenames:
+                if fnmatchcase(base_filename, self):
+                    result.append(base_filename)
+        elif self.__str__() in base_filenames:
+            result.append(self)
+        return result
+
+
+@dataclass
+class FeatureEntry:
+    files: Union[Sequence[FeatureFile], SpecialFileEnum]
+    """The web feature key"""
+    name: str
+
+    _required_keys = {"files", "name"}
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'FeatureEntry':
+        """
+        Converts the provided dictionary to an instance of FeatureEntry
+        :param obj: The object that will be converted to a FeatureEntry.
+        :return: An instance of FeatureEntry
+        :raises ValueError: If there unexpected keys or missing required keys.
+        """
+        assert isinstance(obj, dict)
+        validate_dict(obj, FeatureEntry._required_keys)
+        files = SchemaValue.from_union([
+            lambda x: SchemaValue.from_list(SchemaValue.from_class(FeatureFile), x),
+            SpecialFileEnum], obj.get("files"))
+        name = SchemaValue.from_str(obj.get("name"))
+        return FeatureEntry(files, name)
+
+
+    def does_feature_apply_recursively(self) -> bool:
+        if isinstance(self.files, SpecialFileEnum) and self.files == SpecialFileEnum.RECURSIVE:
+            return True
+        return False
+
+
+@dataclass
+class WebFeaturesFile:
+    """List of features"""
+    features: Sequence[FeatureEntry]
+
+    _required_keys = {"features"}
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'WebFeaturesFile':
+        """
+        Converts the provided dictionary to an instance of WebFeaturesFile
+        :param obj: The object that will be converted to a WebFeaturesFile.
+        :return: An instance of WebFeaturesFile
+        :raises ValueError: If there unexpected keys or missing required keys.
+        """
+        assert isinstance(obj, dict)
+        validate_dict(obj, WebFeaturesFile._required_keys)
+        features = SchemaValue.from_list(FeatureEntry.from_dict, obj.get("features"))
+        return WebFeaturesFile(features)
